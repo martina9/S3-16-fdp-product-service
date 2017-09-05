5eb7eecfb57daf1e1d4e7dcb52d2348b9f1d8468
Martina <m.gabellini9@gmail.com>
2017-09-05 23:00:19 +0200

    Add OrderMessageService and Order Data Access Objects

---
+++ src/main/java/com/fastdeliveryservice/dao/IOrderDAO.java
@@ -0,0 +1,25 @@
+package com.fastdeliveryservice.dao;
+
+import com.fastdeliveryservice.model.Order;
+import com.fastdeliveryservice.model.Order;
+
+import java.util.List;
+
+/**
+ * Created by Martina
+ */
+
+public interface IOrderDAO {
+
+    List<Order> getAllOrders();
+
+    Order getOrderById(int orderId);
+
+    void addOrder(Order order);
+
+    void updateOrder(Order order);
+
+    void deleteOrder(int orderId); 
+
+    List<Order> getOrdersByUserId(int userId);
+}
