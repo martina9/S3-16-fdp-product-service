c62aaa4cc43dce147e374b0e613a5dc6011bad1e
Martina <m.gabellini9@gmail.com>
2017-09-25 21:33:08 +0200

    add unique constraint

--- src/main/java/com/fastdeliveryservice/model/ProductRestaurant.java
+++ src/main/java/com/fastdeliveryservice/model/ProductRestaurant.java
@@ -8,7 +8,7 @@ import java.io.Serializable;
  */
 
 @Entity
-@Table(name="Product_Restaurant")
+@Table(name="Product_Restaurant",indexes = { @Index(name = "IDX_Product_Restaurant", columnList = "RESTAURANT_ID,PRODUCT_ID",unique = true)} )
 public class ProductRestaurant  implements Serializable {
     private static final long serialVersionUID = 1L;
 
